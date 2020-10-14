fe21542a1ca4b24d1bb753f03cdf2f354f19df6d
joway <joway@qq.com>
2020-10-14 14:18:43 +0800

    swoole

--- server.php
+++ server.php
@@ -1,3 +1,22 @@
 <?php
 
 $server = new Swoole\server('127.0.0.1',9501);
+
+//监听链接进入事件
+$server->on('Connect',function ($server,$fd){
+    echo 'Client: Connect.\n';
+});
+
+//监听数据接收事件
+$server->on('Receive',function ($servce,$fd,$from_id,$data){
+   $servce->send($fd,"Server: ".$data);
+});
+
+//监听连接关闭事件
+$server->on('Close',function ($server,$fd){
+   echo "Client: Close.\n";
+});
+
+//启动服务器
+
+$server->start();
