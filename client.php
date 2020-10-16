9d8ab9073e462e70a083ba894647a2b9c14c94e2
joway <joway@qq.com>
2020-10-16 11:56:52 +0800

    client

--- client.php
+++ client.php
@@ -2,7 +2,7 @@
 
 $client = new Swoole\Client(SWOOLE_SOCK_TCP);
 
-if(!$client->connect('62.234.4.156',9502,-1)){
+if(!$client->connect('62.234.4.156',9501,-1)){
     echo 'connect failed error'.$client->errCode;
 }
 $client->send('success');
